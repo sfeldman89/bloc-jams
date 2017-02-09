f12a84881995e62eff83e09b72908f3546ff31b7
sfeldman89 <sfeldman89@gmail.com>
2017-02-08 19:12:39 -0500

    checkpoint-10

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,5 +1,5 @@
-function forEach(points, callback) {
-    for(var i = 0; i < points.length; i++) {
+function forEach(array, callback) {
+    for (var i = 0; i < array.length; i++) {
         callback(array[i]);
     }
 }
