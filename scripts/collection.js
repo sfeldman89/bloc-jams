c7c2b353c875e34dad3bb3e791a28fcb64707683
sfeldman89 <sfeldman89@gmail.com>
2017-02-09 18:32:39 -0500

    checkpoint-10 scripts fix

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,4 @@
- var collectionItemTemplate =
+var collectionItemTemplate =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -15,8 +15,12 @@
    ;
 
  window.onload = function() {
+     // #1
      var collectionContainer = document.getElementsByClassName('album-covers')[0];
+     // #2
      collectionContainer.innerHTML = '';
+ 
+     // #3
      for (var i = 0; i < 12; i++) {
          collectionContainer.innerHTML += collectionItemTemplate;
      }
